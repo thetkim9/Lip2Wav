0bd3566c4c3cb6a9cc37b5810f12930ab4c2ce37
김태수 <tkim9@gimtaesuui-MacBookPro.local>
2020-07-30 17:40:43 +0900

    start server&client

---
+++ server.py
@@ -0,0 +1,192 @@
+from flask import Flask, render_template, request, send_file, make_response
+#from flask_limiter import Limiter
+from PIL import Image, ImageOps
+#from subprocess import Popen, PIPE
+#import shlex
+from moviepy.editor import *
+#import threading
+import time
+import shutil
+import os
+import sys
+#import io
+from demo.utils import *
+
+#import trace
+import threading
+
+progressRates = {}
+threads = []
+
+global model
+model = None
+
+
+class thread_with_trace(threading.Thread):
+    def __init__(self, *args, **keywords):
+        threading.Thread.__init__(self, *args, **keywords)
+        self.killed = False
+
+    def start(self):
+        self.__run_backup = self.run
+        self.run = self.__run
+        threading.Thread.start(self)
+
+    def __run(self):
+        sys.settrace(self.globaltrace)
+        self.__run_backup()
+        self.run = self.__run_backup
+
+    def globaltrace(self, frame, event, arg):
+        if event == 'call':
+            return self.localtrace
+        else:
+            return None
+
+    def localtrace(self, frame, event, arg):
+        if self.killed:
+            if event == 'line':
+                raise SystemExit()
+        return self.localtrace
+
+    def kill(self):
+        self.killed = True
+
+def run_model(user_id):
+
+
+app = Flask(__name__, static_url_path="", template_folder="./")
+app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024 * 8
+
+#limiter = Limiter(app, default_limits=['1 per second'])
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route('/render3D/<int:user_id>', methods=['GET', 'POST'])
+def render3D(user_id):
+  if request.method != "POST":
+    return
+
+  global threads
+  if len(threads)>5:
+      return {'error': 'too many requests'}, 429
+
+  print("hi0", user_id)
+  if not request.files.get('person_image'):
+    return {'error': 'must have a image of human face'}, 400
+
+  try:
+    global progressRates
+    #user_id = int(request.form.get('user_id'))
+    print("hi1", user_id)
+    human_face = Image.open(request.files['person_image'].stream)
+    if(human_face.format not in ['JPG', 'JPEG', 'PNG']):
+      return {'error': 'image must be jpg, jpeg or png'}, 401
+
+    print("hi2", user_id)
+    path = os.path.join("demo/inputs", str(user_id))
+    os.mkdir(path)
+    print("hi3", user_id)
+    dir1 = "demo/inputs/"+str(user_id)+"/"+str(user_id)+"."+human_face.format.lower()
+    human_face.save(dir1)
+    progressRates[user_id] = 10
+    print("hi4", user_id)
+
+    t1 = thread_with_trace(target=run_model, args=[user_id])
+    t1.user_id = user_id
+    threads.append(t1)
+    while threads[0].user_id!=user_id:
+        print(str(user_id)+": ", threads[0].user_id)
+        if threads[0].is_alive():
+            threads[0].join()
+    threads[0].start()
+    threads[0].join(timeout=20)
+    if threads[0].is_alive():
+        threads[0].kill()
+    print(threads.pop(0))
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+
+    progressRates[user_id] = 70
+
+    clip = (VideoFileClip("demo/outputs/"+str(user_id)+"/texture_animation.mp4"))
+    clip.write_gif("demo/outputs/"+str(user_id)+"/outImg.gif")
+    progressRates[user_id] = 90
+    print("hi5", user_id)
+    result = send_file("demo/outputs/"+str(user_id)+"/outImg.gif", mimetype='image/gif')
+    print("hi6", user_id)
+    response = make_response(result)
+    print("hi7", user_id)
+    response.headers['Content-Type'] = str(user_id)
+    print("hi8", user_id)
+    return response
+
+  except Exception:
+    return {'error': 'error while loading input and/or output image files'}, 403
+
+@app.route('/setup/<int:user_id>')
+def setup(user_id):
+    global progressRates
+    progressRates[user_id] = 0
+    return "0"
+
+@app.route('/progress/<int:user_id>')
+def progress(user_id):
+    global progressRates
+    return str(progressRates.get(user_id, 100))
+
+@app.route('/remove/<int:user_id>')
+def remove(user_id):
+    try:
+        for i in range(len(threads)):
+            if threads[i].user_id == user_id and threads[i].is_alive():
+                threads[i].kill()
+                break
+        path = os.path.join("demo/inputs", str(user_id))
+        shutil.rmtree(path)
+        path = os.path.join("demo/outputs", str(user_id))
+        shutil.rmtree(path)
+    except:
+        pass
+    progressRates.pop(user_id, None)
+    return "0"
+
+@app.route('/pending/<int:user_id>')
+def pending(user_id):
+    global threads
+    for i in range(len(threads)):
+        if threads[i].user_id == user_id:
+            return str(i)
+    if progressRates.get(user_id, 100) == 100:
+        return "0"
+    else:
+        return str(len(threads))
+
+@app.errorhandler(413)
+def request_entity_too_large(error):
+  return {'error': 'File Too Large'}, 413
+
+@app.route('/healthz')
+def health():
+  return 200
+
+if __name__ == '__main__':
+    import argparse
+    import demo.demo as demo
+    parser = argparse.ArgumentParser(description='Demo configurations.')
+    parser.add_argument('--input', default='demo/inputs', type=str,
+                        help='Path to the directory containing input images')
+    parser.add_argument('--result', default='demo/outputs', type=str,
+                        help='Path to the directory for saving results')
+    parser.add_argument('--checkpoint', default='./pretrained/pretrained_celeba/checkpoint030.pth', type=str,
+                        help='Path to the checkpoint file')
+    parser.add_argument('--output_size', default=128, type=int, help='Output image size')
+    parser.add_argument('--gpu', default=True, action='store_true', help='Enable GPU')
+    parser.add_argument('--detect_human_face', default=True, action='store_true',
+                        help='Enable automatic human face detection. This does not detect cat faces.')
+    parser.add_argument('--render_video', default=True, action='store_true', help='Render 3D animations to video')
+    args = parser.parse_args()
+    model = demo.Demo(args)
+
+    app.run(debug=False, port=80, host='0.0.0.0', threaded=True)
---
+++ static/request.js
@@ -0,0 +1,115 @@
+var controller;
+document.body.onload = function() {
+    document.getElementById("load").style.visibility = "hidden";
+}
+window.onbeforeunload = function() {
+    if (timer!=null) {
+        clearInterval(timer);
+    }
+    if (controller!=null) {
+        //alert("abort");
+        controller.abort();
+    }
+    if (user_id!=null) {
+        //alert("abort");
+        $.get('remove/' + user_id);
+        console.log("abort");
+    }
+    return "Do you really want to leave this page?";
+}
+var timer;
+var timer2;
+function check_progress(task_id, progress_bar) {
+    document.getElementById("load").style.visibility = "visible";
+    var progress_bar = document.getElementById("progress_bar");
+    var pending = document.getElementById("pending");
+    var dots = document.getElementById("dots");
+    var time_spent = document.getElementById("time");
+    var temp = [".", "..", "..."];
+    var time = 0;
+    function worker() {
+      $.get('progress/' + task_id, function(progress) {
+          progress_bar.value = Math.min(parseInt(progress), 100).toString();
+          time += 1;
+          time_spent.innerHTML = time;
+          dots.innerHTML = temp[time%3];
+          if (parseInt(progress)>=100) {
+            dots.innerHTML = " complete";
+            clearInterval(timer);
+          }
+      })
+    }
+    timer = setInterval(worker, 1000);
+    function worker2() {
+      $.get('pending/' + task_id, function(order) {
+          pending.innerHTML = order;
+          if (parseInt(progress_bar.value)==100) {
+            clearInterval(timer2);
+          }
+      })
+    }
+    timer2 = setInterval(worker2, 3000);
+}
+
+document.getElementById("submit").onclick = () => {
+    document.getElementById("result").src = "";
+    var formData = new FormData();
+    var source = document.getElementById('source').files[0];
+    var submit = document.getElementById('submit');
+    submit.style.visibility = "hidden";
+    //const { v4: uuidv4 } = require('uuid');
+    //var user_id = uuidv4();
+    user_id = Math.floor(Math.random()*1000000000);
+    var progress_bar = document.getElementById('progress_bar');
+    formData.append('person_image', source);
+    //formData.append('user_id', user_id);
+    $.get('setup/' + user_id);
+    check_progress(user_id, progress_bar);
+    controller = new AbortController();
+    var abort = controller.signal;
+    fetch(
+        '/render3D/'+user_id,
+        {
+            method: 'POST',
+            body: formData,
+            signal: abort
+        }
+    )
+    .then(response => {
+        if ( response.status == 200){
+            return response;
+        }
+        else{
+            throw Error("rendering error:");
+        }
+    })
+    .then(response => {
+        var id = -1;
+        for (var pair of response.headers.entries()) {
+            if (pair[0]=="content-type") {
+                id = pair[1]
+                break;
+            }
+        }
+        if (parseInt(id) == user_id) {
+            console.log(id);
+            return response.blob();
+        }
+        else
+            throw Error("response to different user");
+    })
+    .then(blob => URL.createObjectURL(blob))
+    .then(imageURL => {
+        document.getElementById("result").src = imageURL;
+        //document.body.innerHTML += imageURL;
+        document.getElementById("errorbox").innerHTML = "";
+        $.get('remove/' + user_id);
+        submit.style.visibility = "visible";
+    })
+    .catch(e =>{
+        if (e!=Error("response to different user")) {
+            document.getElementById("errorbox").innerHTML = e;
+            $.get('remove/' + user_id);
+        }
+    })
+}
