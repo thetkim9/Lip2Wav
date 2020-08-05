60375aa075f0eee18b43a6f8ca0f778dc1e21389
김태수 <tkim9@gimtaesuui-MacBookPro.local>
2020-08-05 18:30:12 +0900

    bug fix3

--- static/stream.js
+++ static/stream.js
@@ -54,13 +54,6 @@ if (document.getElementById("webcam0")!=null) {
               .catch(log);
     }, false);
 
-    function blobToFile(theBlob, fileName){
-    //A Blob() is almost a File() - it's just missing the two properties below which we will add
-    theBlob.lastModifiedDate = new Date();
-    theBlob.name = fileName;
-    return theBlob;
-}
-
     stopButton.addEventListener("click", function() {
         stop(preview.srcObject)
         .then (recordedChunks => {
